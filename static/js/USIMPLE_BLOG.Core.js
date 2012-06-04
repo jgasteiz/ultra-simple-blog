6c7a02243ad2d86a4641ec6c4c768ce14e9fb38c
javi <javigasteiz89@gmail.com>
2012-06-04 21:02:59 +0200

    Javascript changes
    Nice structure now

---
+++ static/js/USIMPLE_BLOG.Core.js
@@ -0,0 +1,71 @@
+/**
+ * Main class of Ultra Simple Blog - All functions
+ *
+ * @class Core
+ *
+ * @author Javi Manzano | @jgasteiz
+ */
+
+USIMPLE_BLOG.Core = (function() {
+
+	/**
+	 * Init click listeners
+	 *
+	 * @method initListeners
+	 */
+	var initListeners = function() {
+		$(".showFormButton").click(function() {
+			$(".post").toggle();
+			$(".line").toggle();
+			$(".new-entry-actioner").toggle();
+			$(".new-entry-container").toggle();
+		});
+		$(".cancelEditing").click(function() {
+			$(".new-entry-container").animate({
+				width: 'toggle',
+				height: 'toggle'
+			}, {
+				duration: 100,
+				complete: function() {
+					$(".new-entry-actioner").show();
+				}
+			});
+		});
+		$(".icon-anchor").click(function(e) {
+			e.preventDefault();
+		})
+	}
+
+	/**
+	 * Enable edit-mode for an entry
+	 *
+	 * @method enableEditing
+	 *
+	 * @param {int} id
+	 */
+	var enableEditing = function(id) {
+		$(".new-entry-actioner").toggle();
+		$("#post_" + id).toggle();
+		$("#form_" + id).toggle();
+	};
+
+	/**
+	 * Deletes an entry
+	 *
+	 * @method deleteEntry
+	 *
+	 * @param {int} id
+	 */
+	var deleteEntry = function(id) {
+		if(confirm("You're about to delete this entry, do you want to continue?")) {
+			location.href = "delete_entry/" + id;
+		}
+	};
+
+	return {
+		initListeners: initListeners,
+		enableEditing: enableEditing,
+		deleteEntry: deleteEntry
+	};
+
+})(USIMPLE_BLOG);
---
+++ static/js/USIMPLE_BLOG.js
@@ -0,0 +1,11 @@
+/**
+ * Ultra Simple Blog
+ *
+ * @author Javi Manzano | @jgasteiz
+ */
+
+var USIMPLE_BLOG = USIMPLE_BLOG || {};
+
+USIMPLE_BLOG.VERSION = '1.0';
+
+USIMPLE_BLOG.Core || (USIMPLE_BLOG.Core = {});
