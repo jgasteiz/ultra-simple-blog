f31689d2cb4ec92da3703d4511759bbd9d8b8652
javi <javigasteiz89@gmail.com>
2012-06-06 19:58:11 +0200

    Updated templatetags
    imgs weren't loading

--- simpleblog/templatetags/simpleblog.py
+++ simpleblog/templatetags/simpleblog.py
@@ -6,8 +6,8 @@ register = template.Library()
 @register.filter(name='allowtags')
 def allowtags(value):
 	return bleach.clean(value, 
-		tags=bleach.ALLOWED_TAGS + ['b', 'strong', 'em', 'i', 'iframe'], 
-		attributes=bleach.ALLOWED_TAGS + ['width', 'height', 'src', 'frameborder'], 
+		tags=['iframe', 'img', 'a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul'], 
+		attributes={'a': ['href', 'title'], 'acronym': ['title'], 'abbr': ['title'], 'img': ['src', 'alt'], 'iframe': ['width', 'height', 'src', 'frameborder']},
 		strip=True)
 	
 allowtags.is_safe = True
