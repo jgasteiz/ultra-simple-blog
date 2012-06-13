b86b5b9237d3856f2edb007d2099e7cb46affdce
javi <javigasteiz89@gmail.com>
2012-06-13 18:44:35 +0200

    Added "safe" property to markdown template tag and updated djangoappengine
    Today I discovered that I could set a link with
    "javascript:alert('whatever');" and I could solve it by adding a safe
    property to markdown templatetag

--- simpleblog/tests.py
+++ simpleblog/tests.py
@@ -6,12 +6,24 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+# from django.test import TestCase
 
+# class SimpleTest(TestCase):
+#     def test_basic_addition(self):
+#         """
+#         Tests that 1 + 1 always equals 2.
+#         """
+#         self.assertEqual(1 + 1, 2)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from django.utils import unittest
+from simpleblog.models import Entry
+
+class AnimalTestCase(unittest.TestCase):
+    def setUp(self):
+        self.entry1 = Entry(title="Sample Entry1", content="bla bla bla bla bla").put()
+        self.entry2 = Entry(title="Sample Entry2", content="ble ble ble ble ble").put()
+
+    def test_consistency(self):
+        """Should be the same as when created"""
+        self.assertEqual(self.entry1.title, 'Sample Entry1')
+        self.assertEqual(self.entry2.title, 'Sample Entry2')
